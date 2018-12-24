1c31a311b4bbf4bc86b8de489cc9d7a3fdb94f90
Eduardo Rengifo <eduardorengifo5@gmail.com>
2018-12-24 11:13:51 -0500

    Implement styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,9 @@
-export default () => <div>Hello Next.js</div>;
+import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: red;
+  font-size: 50px;
+`
+
+export default () => <Title>My page</Title>
