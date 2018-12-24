ef9297165ae815805349e3d25868c83309ee7dec
Eduardo Rengifo <eduardorengifo5@gmail.com>
2018-12-24 11:43:43 -0500

    Implement eslint config

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-const Koa = require('koa')
-const next = require('next')
-const Router = require('koa-router')
+const Koa = require('koa');
+const nextJS = require('next');
+const Router = require('koa-router');
 const path = require('path');
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev, dir: path.resolve(__dirname, 'src') })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = nextJS({ dev, dir: path.resolve(__dirname, 'src') });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = new Koa()
-  const router = new Router()
+  const server = new Koa();
+  const router = new Router();
 
-  router.get('*', async ctx => {
-    await handle(ctx.req, ctx.res)
-    ctx.respond = false
-  })
+  router.get('*', async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
 
   server.use(async (ctx, next) => {
-    ctx.res.statusCode = 200
-    await next()
-  })
+    ctx.res.statusCode = 200;
+    await next();
+  });
 
-  server.use(router.routes())
+  server.use(router.routes());
   server.listen(port, () => {
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+    // eslint-disable-next-line no-console
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
