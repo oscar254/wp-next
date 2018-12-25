baac63476ae972a81b73b58ecf8c3bd4f13701ea
Eduardo Rengifo <eduardorengifo5@gmail.com>
2018-12-24 20:13:15 -0500

    Add and implement default layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import DefaultLayout from '../layouts/default';
+
 const Title = styled.h1`
   color: red;
   font-size: 50px;
 `;
 
-export default () => <Title>My page</Title>;
+export default () => (
+  <DefaultLayout>
+    <Title>My page</Title>
+  </DefaultLayout>
+);
